4c509fcc945e78c018aae759d1f7cd60af4b14cf
e5ash <e5ash.bro@gmail.com>
2017-06-27 07:15:37 +0300

    End Donwload

--- template/scripts/scripts.js
+++ template/scripts/scripts.js
@@ -1,14 +1,9 @@
 $(document).ready(function($) {
-	$('form').submit(function() {
-		$.ajax({
-		    type: "POST",
-		    url: "/order.php",
-		    data: $(this).serialize()
-		}).done(function() {
-		    popupForm.css('display','none');
-		    popupMessage.css('display','block');
-		});
-		return false;
+	$('.gallery__list').slick({
+		slidesToShow: 1,
+		centerMode: true,
+		variableWidth: true,
+		appendArrows: $('.gallery__arrows')
 	});
 });
 
